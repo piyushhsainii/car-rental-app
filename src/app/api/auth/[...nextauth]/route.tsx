fb5383df87dc56d462772d4a36050418e6f96703
Piyush <sainipiyush8860@gmail.com>
2024-03-15 10:23:58 +0530

    Removed export keyword

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -42,7 +42,7 @@ export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 }
 
-export const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
 
 
